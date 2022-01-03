6bf573c6fd0e13ea7ba4c9fac76234cf351944cd
Jatin Thakwani <47001242+jatin1408@users.noreply.github.com>
2022-01-03 13:46:08 +0530

    Time: 4 ms (50.79%), Space: 56.1 MB (42.94%) - LeetHub

--- maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.java
+++ maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.java
@@ -1,68 +1,27 @@
 class Solution {
     public int getMaxLen(int[] nums) {
-        int negative=0;
-        int len=0;
-        int f=-1;
-        int l=-1;
-        int n=nums.length;
         int lastZero=-1;
+        int firstNegative=-1;
+        int countNegative=0;
+        int max=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]<0) {
-                if(f==-1) f=i;
-                l=i;
-                negative++;
+            if(nums[i]<0){
+                countNegative++;
+                if(firstNegative==-1) firstNegative=i; 
             }
-            else if(nums[i]==0){
-                if(negative%2==0) {
-                    int ans=i-lastZero - 1;
-                    len=Math.max(len,ans);
-                    lastZero=i;
-                    f=-1;
-                    l=-1;
-                    continue;
-                }
-                int ll=i-l;
-                int fl=f-lastZero;
-                if(fl<ll){
-                    f++;
-                    int ans=i-f;
-                    len=Math.max(len,ans);
-                }
-                else{
-                    l--;
-                    int ans=l-lastZero;
-                    len=Math.max(len,ans);
-                }
+            
+            if(nums[i]==0){
+                countNegative=0;
+                firstNegative=-1;
                 lastZero=i;
-                f=-1;
-                l=-1;
-                negative=0;
-                
+            }
+            
+            else{
+                if(countNegative%2==0) max=Math.max(max,i-lastZero);
+                else max=Math.max(max,i - firstNegative);
             }
         }
-        if(f!=-1 || l!=-1){
-            if(negative%2==0){
-                int ans=n-lastZero -1;
-                    len=Math.max(len,ans);
-                    return len;
-            }
-        int ll=n-l;
-        int fl=f-lastZero;
-        if(fl<ll){
-            f++;
-            int ans=n-f;
-            len=Math.max(len,ans);
-        }
-        else{
-            l--;
-            int ans=l-lastZero;
-            len=Math.max(len,ans);
-        }
-        }
-        else{
-            int ans=n-lastZero -1;
-            len=Math.max(len,ans);
-        }
-        return len;
+        return max;
     }
 }
+    
