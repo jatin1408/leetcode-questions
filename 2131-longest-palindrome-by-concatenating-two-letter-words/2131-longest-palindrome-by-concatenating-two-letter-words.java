27953ca8052d1118ab21bc2fe19c5f199f2c1c01
Jatin Thakwani <47001242+jatin1408@users.noreply.github.com>
2022-01-08 21:39:44 +0530

    Time: 287 ms (25.00%), Space: 122 MB (25.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -3,28 +3,29 @@ class Solution {
         int ans=0;
         Map<String,Integer> map=new HashMap<>();
         for(String word:words) map.put(word,map.getOrDefault(word,0) + 1);
+        Set<String> set=new HashSet<>();
         boolean found=false;
         for(String word:words){
             String reversed="";
             reversed+=word.charAt(1);
             reversed+=word.charAt(0);
-            if(reversed.equals(word) && map.containsKey(word)) {
+            if(reversed.equals(word) && !set.contains(word)) {
                 int num = map.get(word);
                 if(num%2!=0){
                     found=true;
                     num--;
                 }
                 ans += num * 2;
-                map.remove(word);
+                set.add(word);
             }
 
-            else if(map.containsKey(reversed)){
+            else if(map.containsKey(reversed) && !set.contains(word)){
                 int ff=map.get(word);
                 int sf=map.get(reversed);
                 int min=Math.min(ff,sf);
                 ans+=min*4;
-                map.remove(reversed);
-                map.remove(word);
+                set.add(word);
+                set.add(reversed);
             }
         }
         if(found) ans+=2;
