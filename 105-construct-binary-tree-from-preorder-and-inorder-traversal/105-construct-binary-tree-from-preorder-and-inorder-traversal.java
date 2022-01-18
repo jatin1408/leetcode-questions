63bf0acce7a096f8a23f10b26abccc789931be6e
Jatin Thakwani <47001242+jatin1408@users.noreply.github.com>
2022-01-19 01:42:27 +0530

    Time: 3 ms (63.85%), Space: 38.8 MB (88.08%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int index=0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return create(preorder,inorder,0,preorder.length);
+    }
+    public TreeNode create(int[] preorder,int[] inorder,int low,int high){
+         if(low>high || index>=preorder.length) return null;
+        if(low==high) return new TreeNode(preorder[index++]);
+      
+        int val=preorder[index++];
+        int targetIndex=findIndex(inorder,val);
+        TreeNode root=new TreeNode(val);
+        root.left=create(preorder,inorder,low,targetIndex-1);
+        root.right=create(preorder,inorder,targetIndex+1,high);
+        return root;
+    }
+    public int findIndex(int[] inorder,int target){
+        for(int i=0;i<inorder.length;i++) if(inorder[i]==target) return i;
+        return -1;
+    }
+}
