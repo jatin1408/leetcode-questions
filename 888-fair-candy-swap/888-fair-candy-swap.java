0f0d6eb046cfc4aa2c5664e919dc29b626eb170e
Jatin Thakwani <47001242+jatin1408@users.noreply.github.com>
2022-01-25 18:51:30 +0530

    Time: 19 ms (45.87%), Space: 69.4 MB (9.45%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.java
+++ 888-fair-candy-swap/888-fair-candy-swap.java
@@ -3,15 +3,14 @@ class Solution {
     public int[] fairCandySwap(int[] al, int[] bob) {
         int sum1=calcSum(al,true);
         int sum2=calcSum(bob,false);
-        int total=sum1+sum2;
-        int half=total/2;
+        int half=(sum1+sum2)/2;
         int[] res=new int[2];
         for(int ele:bob){
             int s1 = half - (sum2 - ele);
             if(set.contains(s1)) {
                 res[0] = s1;
                 res[1] = ele;
-                 break;   
+                break;   
             }
         }   
         return res;
@@ -21,7 +20,7 @@ class Solution {
         for(int ele:arr){
            if(add) set.add(ele);
           sum+=ele;  
-        } 
+         } 
         return sum;
     }
 }
